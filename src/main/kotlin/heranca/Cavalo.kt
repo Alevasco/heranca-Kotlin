ff42a1418d66f069230ece786d27d8c94d8f7a1e
Alevasco <ale_vasconcelos96@hotmail.com>
2022-03-08 15:06:29 -0300

    Exercicios Heranca

---
+++ src/main/kotlin/heranca/Cavalo.kt
@@ -0,0 +1,18 @@
+package heranca
+
+open class Cavalo(
+    nome: String, idade: Int
+
+) : Animal(nome, idade) {
+
+
+    override fun correr() {
+        println("Correndo")
+    }
+
+
+     override fun emitirSom(){
+         println("Relinchando")
+     }
+}
+
